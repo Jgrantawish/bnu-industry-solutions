fe6c07abc33d4b9755844f3667fe43048022a6e2
Jgrantawish <joannaindiagrant@gmail.com>
2026-03-25 21:42:20 +0000

    added Add Supplier function and helper getValidString functions

--- src/main/Main.java
+++ src/main/Main.java
@@ -63,10 +63,30 @@ public class Main {
 
     }
    
-    private static int getMainMenuOption(Scanner scanner){
-        String menuText= "Please select the corresponding number (1-6) from the menu below:\n 1. Supplier Management\n 2. Customer Management\n 3. Inventory Management \n 4. Order Management \n 5. Finance Management \n 6. Exit";
-        System.out.println("\n----- WHMS ----- \n\nWhich area of the WHMS would you like to access?\n" + menuText);
-        return getNumberOption(scanner, 6, menuText);
+    private static String getStringInput(Scanner scanner, String prompt){
+        System.out.println(prompt);
+        return scanner.nextLine();
+    }
+
+    private static String getEmailInput(Scanner scanner, String prompt){
+        System.out.println(prompt);
+        String input = scanner.nextLine();
+        while (!input.contains("@")) {
+            System.out.println("Uh Oh! You did not enter an email address containing an '@' symbol. " + prompt);
+            input = scanner.nextLine();
+        }
+        return input;
+    }
+
+    private static String getPhoneNumberInput(Scanner scanner, String prompt){
+        System.out.println(prompt);
+        String input = scanner.nextLine();
+        // Regex for strings containing digits 0 to 9, +, - and spaces
+        while (!input.matches("[0-9+\\- ]+")){
+            System.out.println("Uh Oh! You entered a phone number that contained letters or other symbols. " + prompt);
+            input = scanner.nextLine();
+        }
+        return input;
     }
 
     private static int getNumberOption(Scanner scanner, int maxMenuLength, String menuText){
@@ -88,8 +108,14 @@ public class Main {
         return option;
     }
 
+    private static int getMainMenuOption(Scanner scanner){
+        String menuText= "Please select the corresponding number (1-6) from the menu below:\n 1. Supplier Management\n 2. Customer Management\n 3. Inventory Management \n 4. Order Management \n 5. Finance Management \n 6. Exit";
+        System.out.println("\n----- WHMS ----- \n\nWhich area of the WHMS would you like to access?\n" + menuText);
+        return getNumberOption(scanner, 6, menuText);
+    }
+
     private static int getSupplierManagementMenuOption(Scanner scanner){
-        String menuText= "Please select the corresponding number (1-5) from the menu below:\n 1. View All Suppliers\n 2. Create New Supplier\n 3. Delete Supplier \n 4. Update Supplier Details \n 5. Back to Main Menu";
+        String menuText= "Please select the corresponding number (1-5) from the menu below:\n 1. View All Suppliers\n 2. Add New Supplier\n 3. Delete Supplier \n 4. Update Supplier Details \n 5. Back to Main Menu";
         System.out.println("\n----- Supplier Management ----- \n\nWhat would you like to do?\n" + menuText);
         return getNumberOption(scanner, 5, menuText);
     }
@@ -100,7 +126,7 @@ public class Main {
                 displayAllSuppliers();
             break;
             case 2:
-            //
+                addNewSupplier(scanner);
             break;
             case 3:
             //
@@ -122,6 +148,23 @@ public class Main {
         for (Supplier supplier : suppliers){
             index++;
             System.out.println(index + ". " + supplier.toString());
+        }
+    }
+
+    private static void addNewSupplier(Scanner scanner){
+        System.out.println("\n----- Add New Supplier -----");
+        String name = getStringInput(scanner, "Please enter the Supplier's name.");
+        String email = getEmailInput(scanner, "Please enter the Supplier's email address.");
+        String phone = getPhoneNumberInput(scanner, "Please enter the Supplier's phone number.");
+        String address = getStringInput(scanner, "Please enter the Supplier's address.");
+        String postcode = getStringInput(scanner, "Please enter the Supplier's postcode.");
+
+        try {
+            WHMS.supplierManager.addSupplier(name, email, phone, address, postcode);
+            System.out.println("Your new Supplier " + name + " has been successfully added!");
+        } 
+        catch (Exception e){
+            System.out.println("Uh Oh! Something went wrong. We could not add your new Supplier to the system.");
         }
     }
 
