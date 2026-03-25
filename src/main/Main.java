d5c4360d31eaf9038cfe848811850e7f1f47e615
Jgrantawish <joannaindiagrant@gmail.com>
2026-03-25 21:51:24 +0000

    recursion fix

--- src/main/Main.java
+++ src/main/Main.java
@@ -138,7 +138,7 @@ public class Main {
             //
             break;
         }
-        getSupplierManagementMenuOption(scanner);
+        handleSupplierManagementMenuOption(scanner);
     }
 
     private static void displayAllSuppliers(){
