d980ae109dd64201098217fa44a90fd853e917d1
Jgrantawish <joannaindiagrant@gmail.com>
2026-04-08 08:15:21 +0100

    Commented Finance related classes

--- src/main/FinanceManager.java
+++ src/main/FinanceManager.java
@@ -1,6 +1,9 @@
 import java.time.Month;
 import java.util.ArrayList;
 
+/** 
+ * The FinanceManager Class provides overall statistics about the company's monthly financial position. 
+*/
 public class FinanceManager {
     private OrderManager<SupplierOrder> supplierOrderManager;
     private OrderManager<CustomerOrder> customerOrderManager;
@@ -10,16 +13,20 @@ public class FinanceManager {
         this.customerOrderManager = customerOrderManager;
     }
 
+    /** Returns the total number of Supplier Orders placed in a given month.*/
     private int numberOfSupplierOrdersForMonth(int monthNumber){
         ArrayList<SupplierOrder> supplierOrders = this.supplierOrderManager.getOrdersForGivenMonth(monthNumber);
         return supplierOrders.size();
     }
 
+    /** Returns the total number of Customer Orders placed in a given month. */
     private int numberOfCustomerOrdersForMonth(int monthNumber){
         ArrayList<CustomerOrder> customerOrders = this.customerOrderManager.getOrdersForGivenMonth(monthNumber);
         return customerOrders.size();
     }
 
+
+    /** Returns the total amount that customers paid for products in a given month.*/
     private double calculateIncomeForMonth(int monthNumber){
         double totalIncome = 0;
         ArrayList<CustomerOrder> customerOrders = this.customerOrderManager.getOrdersForGivenMonth(monthNumber);
@@ -29,6 +36,7 @@ public class FinanceManager {
         return totalIncome;
     }
 
+    /** Returns the total amount that BNU Industry Solutions paid suppliers in a given month.*/
     private double calculateExpensesForMonth(int monthNumber){
         double totalExpenses = 0;
         ArrayList<SupplierOrder> supplierOrders = this.supplierOrderManager.getOrdersForGivenMonth(monthNumber);
@@ -38,12 +46,14 @@ public class FinanceManager {
         return totalExpenses;
     }
 
+    /** Uses the income and expenses to calculate the net income for a given month.*/
     private double calculateNetIncomeForMonth(int monthNumber){
         double income = calculateIncomeForMonth(monthNumber);
         double expenses = calculateExpensesForMonth(monthNumber);
         return (income - expenses);
     }
 
+    /** Uses the net income to determine whether the company made a profit, loss, or broke even in a given month.*/
     private FinancialStatus calculateFinancialStatusForMonth(int monthNumber){
         double netIncome = calculateNetIncomeForMonth(monthNumber);
         if (netIncome > 0 ){
@@ -55,6 +65,7 @@ public class FinanceManager {
 
     }
 
+    /** Gives a summary of all of the finanical statistics for a given month.*/
     public String generateMonthlyReport(int monthNumber){
         return "\n----- Financial Summary for " + Month.of(monthNumber) + " -----\n\n Number of Supplier Orders: " + this.numberOfSupplierOrdersForMonth(monthNumber) 
         + "\n Total Outgoings: £" + this.calculateExpensesForMonth(monthNumber) + "\n Number of Customer Orders: " + this.numberOfCustomerOrdersForMonth(monthNumber)
--- src/main/FinancialStatus.java
+++ src/main/FinancialStatus.java
@@ -1,3 +1,6 @@
+ /**
+ * The FinancialStatus class is used to return whether the company has made a profit, loss or broken even.
+ */
 enum FinancialStatus {
     PROFIT, 
     BREAK_EVEN,
